11258931e973370775d3b3cec55673dfa5cbc058
Rodolfo Nascimento Rodrigues <rodolfonr86@gmail.com>
2023-08-20 18:38:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector('.container-cards').addEventListener('click',(event)=>{
+    if (event.target.tagName === "IMG"){
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+        // const modalImg = document.createElement('img');
+        // modalImg.src = event.target.src;
+        // modal.appendChild(modalImg);
+        // document.body.appendChild(modal);
+        // modal.addEventListener('click', ()=>{
+        //     document.body.removeChild(modal)
+        // })
+    }
+})
